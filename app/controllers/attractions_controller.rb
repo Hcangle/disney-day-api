9fe6980708908d119632189d39055fc0fa525552
Hangle <hannahcangle@gmail.com>
2020-11-30 18:58:49 -0600

    Building Destroy and Create

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,14 +4,24 @@ class AttractionsController < ApplicationController
         render :json => Attraction.all, :include => :category
         
     end 
-
+    
+    def create
+       category = Category.find_by(name: params[:category].downcase)
+       attraction = Attraction.create(name: params[:name], category: category)
+       render :json => attraction
+    end 
+    
     def destroy 
-         Attraction.find(params[:id]).destroy
-        #  redirect_to '/attractions'
-        render :json => {id: params[:id]}
+         attraction = Attraction.find(params[:id])
+         render :json => {id: params[:id], message: "Record was successfully deleted."}
+         binding.pry
+         #  if attraction.destroy
+        #     render :json => {id: params[:id], message: "Record was successfully deleted."}
+        #  else 
+        #     render :json => {message: "There was an error."}
+        #  end 
     end
 
-    def create 
+    
 
-    end 
 end
