968f35d3d9f0330158ff92de6979ca64fc88c9fe
Hangle <hannahcangle@gmail.com>
2020-12-02 13:54:04 -0600

    Updated seed file with drop down lists

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,20 +1,20 @@
 class AttractionsController < ApplicationController
 
     def index 
-        render :json => Attraction.all, :include => :category
+        render :json => Attraction.all, :include => :category, :status => 200
         
     end 
     
     def create
        category = Category.find_by(name: params[:category].downcase)
        attraction = Attraction.create(name: params[:name], category: category)
-       render :json => attraction
+       render :json => attraction.get_category
     end 
     
     def destroy 
          attraction = Attraction.find(params[:id])
-         render :json => {id: params[:id], message: "Record was successfully deleted."}
-         binding.pry
+         render :json => {id: params[:id], message: "Record was successfully deleted."}, :status => 200 
+         
          #  if attraction.destroy
         #     render :json => {id: params[:id], message: "Record was successfully deleted."}
         #  else 
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,3 +1,7 @@
 class Attraction < ApplicationRecord
     belongs_to :category
+
+    def get_category
+     {"name" => self.name, "category" => self.category}
+    end 
 end
